91299a871130dc800c27aae2b92cc5b6df40b201
JonCal2024 <49603100+JonCal2024@users.noreply.github.com>
2020-04-20 15:18:26 -0700

    Worked on Server UI

--- GameServer/src/UI/GameDetailsController.java
+++ GameServer/src/UI/GameDetailsController.java
@@ -1,4 +1,79 @@
 package UI;
 
-public class GameDetailsController {
+import DataClasses.TTT_GameData;
+import GameInterfaces.Game;
+import TicTacToe.TTT_Game;
+import javafx.application.Platform;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GameDetailsController implements Initializable
+{
+    public GridPane board;
+    public Button previousButton;
+    public Button nextButton;
+    public Button cancelButton;
+    public Label moveNumLabel;
+    public Label playerTurnLabel;
+    public Label player1Label;
+    public Label player2Label;
+    public Label startTimeLabel;
+    public Label endTimeLabel;
+    public Label winnerLabel;
+    public Label moveTimeLabel;
+    public ListView spectatorsList;
+    private TTT_GameData gameData;
+    private TTT_Game game;
+
+    public void onCancelClicked()
+    {
+        try
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Server.fxml"));
+            Parent root = loader.load();
+            ServerController sc = loader.getController();
+            Stage stage = (Stage) cancelButton.getScene().getWindow();
+            stage.close();
+            stage.setTitle("Server");
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void passInfo(TTT_Game game, TTT_GameData gameData)
+    {
+        this.gameData = gameData;
+        this.game = game;
+
+        Platform.runLater(() -> {
+            startTimeLabel.setText(gameData.getStartingTime().toString());
+            endTimeLabel.setText(gameData.getEndTime().toString());
+
+            int x = game.getMoveHistory().get(0).getRow();
+            int y = game.getMoveHistory().get(0).getColumn();
+            board.add(new Label("X"), y, x);
+
+            //set other fxml elements
+            previousButton.setDisable(true);
+        });
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
 }
--- GameServer/src/UI/ServerController.java
+++ GameServer/src/UI/ServerController.java
@@ -1,4 +1,100 @@
 package UI;
 
-public class ServerController {
+import DataClasses.User;
+import GameInterfaces.Game;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class ServerController implements Initializable
+{
+    public ListView registeredPlayersList;
+    public Button modifyPlayerButton;
+    public ListView activeGamesList;
+    public Button activeGameDetailsButton;
+    public ListView inactiveGamesList;
+    public Button inactiveGameDetailsButton;
+    private List<User> playerList;
+    private List<Game> activeGameList;
+    private List<Game> inactiveGameList;
+
+    public void onModifyPlayerClicked()
+    {
+        int usernameIndex = registeredPlayersList.getSelectionModel().getSelectedIndex();
+        User selectedPlayer = playerList.get(usernameIndex);
+        try
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("ModifyPlayer.fxml"));
+            Parent root = loader.load();
+            ModifyPlayerController mpc = loader.getController();
+            mpc.passInfo(selectedPlayer);
+            Stage stage = (Stage) modifyPlayerButton.getScene().getWindow();
+            stage.close();
+            stage.setTitle("Modify Player");
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void onActiveGameDetailsClicked()
+    {
+        try
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("GameDetails.fxml"));
+            Parent root = loader.load();
+            GameDetailsController gdc = loader.getController();
+            //gdc.passInfo();
+            Stage stage = (Stage) activeGameDetailsButton.getScene().getWindow();
+            stage.close();
+            stage.setTitle("Active Game Details");
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    public void onInactiveGameDetailsClicked()
+    {
+        try
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("GameDetails.fxml"));
+            Parent root = loader.load();
+            GameDetailsController gdc = loader.getController();
+            //gdc.passInfo();
+            Stage stage = (Stage) inactiveGameDetailsButton.getScene().getWindow();
+            stage.close();
+            stage.setTitle("Inactive Game Details");
+            stage.setScene(new Scene(root));
+            stage.show();
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        //query for listview data and update UI
+    }
 }
